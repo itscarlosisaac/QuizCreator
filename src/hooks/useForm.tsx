891533d5a3ad62bf485e54dabd351ddc58faafef
Carlos <itscarlosisaac@gmail.com>
2021-07-02 13:15:22 -0400

    Added use form hook.

---
+++ src/hooks/useForm.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react"
+
+type FormInputElements = HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement;
+
+export const useForm = (initialState: any = {}) => {
+  const [formValues, setFormValues] = useState(initialState);
+
+  const reset = () => setFormValues(initialState);
+
+  const handleInputChange = ( target: FormInputElements ) => {
+    setFormValues({
+      ...formValues,
+      [target.name]: target.value
+    })
+  }
+
+  return [ formValues, handleInputChange, reset]
+
+}
