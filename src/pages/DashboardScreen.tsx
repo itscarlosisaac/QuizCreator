e4209ba8208ca7eeb9b6b92f89f55cd2a8466f86
Carlos <itscarlosisaac@gmail.com>
2021-07-09 19:46:24 -0400

    Updated main screens.

--- src/pages/DashboardScreen.tsx
+++ src/pages/DashboardScreen.tsx
@@ -1,35 +1,60 @@
-import { Container, Heading,  Flex  } from "@chakra-ui/react"
+import { useEffect, useState, Suspense } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory, useLocation } from "react-router-dom";
+import { Container, Heading,  Flex, Button  } from "@chakra-ui/react"
 import { Header } from "../components/Header"
 import { ListQuest } from "../components/Questionnaires/ListQuest"
-import { Empty } from "../components/Questionnaires/Empty";
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { firebaseManager } from '../firebase/FirebaseManager';
+import { StartFetchingForms, StartCreatingNewForm } from "../redux/actions/form.actions";
+import { LoadingScreen } from "./LoadingScreen";
+import { Loading } from "../components/common/Loading";
 
 export const DashboardScreen = () => {
 
-  const [state, setstate] = useState<any[]>([])
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const formList = useSelector((state: any) => state.form.formList);
+  const [isLoadingForms, setIsLoadingForms] = useState<boolean>(false)
+  const [isCreating, setIsCreating] = useState<boolean>(false)
 
   useEffect(() => {
-    const questionnaires = firebaseManager.formManager.StartFetchingForms("newForm");
-    questionnaires.then(data => {
-      setstate(data)
-    })
+    setIsLoadingForms(true)     
+    const InternalFetch = async () => {
+      await dispatch(StartFetchingForms("newForm"))
+    };
+    InternalFetch();
+    setIsLoadingForms(false)
   }, [])
 
+  const handleCreateNewForm = async () => {
+    setIsCreating(true)
+    const id = await dispatch(StartCreatingNewForm("newForm"))
+    history.push(`/app/builder/${id}`)
+  }
+
+  if (isLoadingForms) {
+    return <LoadingScreen/>
+  }
   
   return (
     <>
       <Header />
       <Container minW="100vw" minH="100vh" bg="gray.100" >
         <Container maxW="container.xl">
-          <Flex>
-            <Heading my="5" as="h1" size="xl" >
-              You have {state.length} Forms.
+          <Flex justify="space-between" align="center" py="4">
+            <Heading my="5" as="h1" size="md" >
+              You have {formList.length} Forms.
             </Heading>
+            <Button
+              isLoading={isCreating}
+              onClick={handleCreateNewForm}
+              size="md"
+              colorScheme="linkedin">
+              Create New Form
+            </Button>
           </Flex>
-          <Empty/>
-          {/* <ListQuest/> */}
+          <Suspense fallback={<Loading />}>
+            <ListQuest formList={formList} />
+          </Suspense>
         </Container>
       </Container>
     </>
--- src/pages/PublicFormScreen.tsx
+++ src/pages/PublicFormScreen.tsx
@@ -1,12 +1,72 @@
-
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Container, Heading } from "@chakra-ui/react";
+import { useHistory } from "react-router";
+import { v4 as uuid_v4 } from 'uuid';
+
+
+import { Header } from "../components/Header"
+import { PublicFooter } from "../components/Public/PublicFooter";
+import { PublicForm } from "../components/Public/PublicForm";
+import { StartFetchingSingle } from '../redux/actions/form.actions';
+import { firebaseManager} from '../firebase/FirebaseManager';
+import { useForm } from '../hooks/useForm';
 
 export const PublicFormScreen = () => {
+
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const QuestionData = useSelector((state:any) => state.form.public)
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [formData, setFormData] = useForm();
+
+  useEffect(() => {
+    const Single = async () => {
+      setIsLoading(true);
+      await dispatch(StartFetchingSingle( history.location.pathname.split("/")[2])) as any
+      setIsLoading(false);
+    }
+    Single();
+  }, [])
+  
+  const handleResponse = (event: React.FormEvent) => {
+    event.preventDefault()
+    console.log("Submitting")
+    firebaseManager.answerManager.StartSavingAsnwer({
+      questionnarie: QuestionData,
+      answers: {
+        meta: {
+          id: uuid_v4(),
+          user: formData.email,
+          createdAt: new Date(),
+        },
+        data: formData
+      }
+    });
+  }
+
+  const handleChange = (event: React.FormEvent) => {
+    event.preventDefault()
+    setFormData(event.target as HTMLInputElement)
+  }
+
   return (
-    <Container minW="100vw" minH="100vh" bg="gray.100" >
-      <Container maxW="container.xl">
-        <Heading>Public Form </Heading>
+    <>
+      <Header />
+      <Container minW="100vw" minH="100vh" bg="gray.100" >
+        <Container maxW="container.xl">
+          <Heading>Public Form </Heading>
+          <form onSubmit={handleResponse}>
+            {
+              !isLoading && QuestionData &&
+              <>
+                <PublicForm handleChange={handleChange} questions={QuestionData.questionList } />
+                <PublicFooter handleChange={handleChange}/>
+              </>
+            }
+          </form>
+        </Container>
       </Container>
-    </Container>
+    </>
   )
 }
