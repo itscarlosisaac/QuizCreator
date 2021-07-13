a61cf22984f4fac3499e3baa7e936e3d98884cd3
Carlos <itscarlosisaac@gmail.com>
2021-07-12 22:51:08 -0400

    Added Thank you screen.

--- src/pages/ThankYouScreen.tsx
+++ src/pages/ThankYouScreen.tsx
@@ -1,10 +1,8 @@
-import { Container, Box, Heading, Flex, Text } from "@chakra-ui/react"
-import { Header } from "../components/Header"
+import { Container, Box, Heading, Flex, Text } from "@chakra-ui/react";
 
 export const ThankYouScreen = () => {
   return (
     <>
-      <Header />
       <Container minW="100vw" minH="100vh" bg="gray.100" >
         <Container maxW="container.xl">
           <Flex align="center" justify="center">
